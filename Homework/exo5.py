bd19892ce4890ad9eba8d55bb6570a1d1d80dc9b
chaima-la <140322984+chaima-la@users.noreply.github.com>
2024-11-04 06:34:12 -1100

    Delete Homework directory

--- Homework/exo5.py
+++
@@ -1,6 +0,0 @@
-import re
-def extract_hashtag(text):
-    return re.findall(r"\B#\w+",text)
-
-
-print(extract_hashtag("dsjfjg #hjg #jsdgf #ksdhgf ksdj"))
--- Homework/try.py
+++
@@ -1,6 +0,0 @@
-import re
-
-s = "abc hjgb wxcbbbbnf"
-match = re.findall(r"b?\w*",s)
-
-print(match)
